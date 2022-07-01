bc588928ddd6169844a267cc493d61c64d84628d
Pedro-Omo <107281005+Pedro-Omo@users.noreply.github.com>
2022-07-02 00:31:09 +0100

    Add files via upload

---
+++ test6.java
@@ -0,0 +1,8 @@
+public class test6 {
+     public static void main(String[] arg) {
+        System.out.println("Welcome");
+        System.out.println("Unlimited");
+        System.out.println("Possibilties");
+    }
+    
+}
